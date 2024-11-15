291cea23fd89747275b434668956b20a69fdc5f3
minhthuan1809 <thuan1809minh@gmail.com>
2024-11-16 02:03:12 +0700

    f8

--- src/redux/middlewares/admin/decentralization.js
+++ src/redux/middlewares/admin/decentralization.js
@@ -3,34 +3,26 @@ import { DecentralizationAction } from "../../action/admin/decentralization";
 export const getDecentralization = (apikey) => {
   return async (dispatch) => {
     try {
-      const url = `https://3.106.181.73/backend-fastfood/main_admin.php/admin/role`;
+      const url = `${import.meta.env.VITE_FASTFOOD_ADMIN_API}/admin/role`;
       const response = await fetch(url, {
-        method: "POST",
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
           "X-Api-Key": apikey,
         },
-        body: JSON.stringify({
-          action: "get_role",
-        }),
       });
 
       const data = await response.json();
-      console.log("data", data);
-      if (data.success) {
+      if (response.ok) {
         toast.dismiss();
         toast.success(data.message);
         dispatch(DecentralizationAction(data.roles));
-      } else {
-        toast.error(
-          data.message || "Có lỗi xảy ra khi lấy thông tin phân quyền"
-        );
       }
 
       return data;
     } catch (error) {
-      console.error("Lỗi khi gọi API:", error.message);
-      toast.error("Có lỗi xảy ra khi lấy thông tin phân quyền");
+      console.error("Fetch error:", error.message);
+
       throw error;
     }
   };
