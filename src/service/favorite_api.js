6146783a4cf930a0de3a83996b66966f532f87f5
minhthuan1809 <Thuan1809minh@gmail.com>
2024-11-30 11:40:51 +0700

    cancel pass

--- src/service/favorite_api.js
+++ src/service/favorite_api.js
@@ -1,6 +1,6 @@
 export const renderFavorite = async (user_id) => {
   const response = await fetch(
-    `${process.env.VITE_FASTFOOD_SERVER_API}/favorites/${user_id}`
+    `${import.meta.env.VITE_FASTFOOD_SERVER_API}/favorites/${user_id}`
   );
   const data = await response.json();
   console.log(data);
